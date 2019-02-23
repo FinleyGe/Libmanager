40d4fc521c7ed48c312f141f5f455c9f36419e65
Forever-Jimmy <32237950+Forever-Jimmy@users.noreply.github.com>
2019-02-23 19:57:48 +0800

    基本架构

---
+++ Server/Libmanager_server.py
@@ -0,0 +1,5 @@
+# _*_ coding:utf-8 _*_
+# Creator : 葛骏
+# Overview : 服务端主文件
+print("Server if running!")
+
---
+++ Server/model/database.py
@@ -0,0 +1,7 @@
+# _*_ coding:utf-8 _*_
+# Creator : 葛骏
+# Overview : 数据库操作文件
+import sqlite3
+
+class Database():
+    pass
